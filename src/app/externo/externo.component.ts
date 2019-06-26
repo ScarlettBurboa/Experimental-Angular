83d53755b04240acac349e463a126c8e0746a018
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-06-25 22:06:00 -0400

    servicios y httpClient

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PeticionesService } from './../services/peticiones.service';
 @Component({
   selector: 'app-externo',
   templateUrl: './externo.component.html',
-  styleUrls: ['./externo.component.css']
+  styleUrls: ['./externo.component.css'],
+  providers: [PeticionesService]
 })
 export class ExternoComponent implements OnInit {
+  public users: any;
+  public userId: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _peticionesService: PeticionesService) {
+    this.userId = 1;
   }
 
+  ngOnInit() {
+    this.cargaUsuario();
+  }
+  cargaUsuario() {
+    this._peticionesService.getUser(this.userId).subscribe(
+      result => {
+        this.users = result.data;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
 }
